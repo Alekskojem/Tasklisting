dc58977c6b5d243189477fc26536f92859a6064b
Alekskojem <alekskojem@gmail.com>
2018-07-31 22:04:52 +0300

    Add partial file and date time for complete task

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,26 +1,29 @@
 class ItemsController < ApplicationController
 before_action :find_item, only: [:show, :edit, :update, :destroy]
 
-def index
-	@items = Item.all.order("created_at DESC")
+ def index
+	if user_signed_in?
+	@items = Item.where(:user_id => current_user).order("created_at DESC")
+ end
 end
-def show
+	
+	def show
+	end
 
-end
-
-
-def new
-	@item = Item.new
-end
-def create
-	@item = Item.new(item_params)
-	if @item.save
-	redirect_to root_path
-	else
-	render 'new' 
-end
-end
-def edit
+	def new
+		@item = current_user.items.build
+	end
+	
+	def create
+		@item = current_user.items.build(item_params)
+		if @item.save
+			redirect_to root_path
+		else
+			render 'new' 
+		end
+	end
+	
+	def edit
 	end
 
 	def update
@@ -36,13 +39,19 @@ def edit
 		redirect_to root_path
 	end
 
-private
-
-	def item_params
-		params.require(:item).permit(:title, :description)
-	end
-
-	def find_item
-		@item = Item.find(params[:id])
-	end
+def complete
+	@item = Item.find(params[:id])
+	@item.update_attribute(:completed_at, Time.now)
+	redirect_to root_path
 end
+
+	private
+
+		def item_params
+			params.require(:item).permit(:title, :description)
+		end
+
+		def find_item
+			@item = Item.find(params[:id])
+		end
+	end
